6d75fc6e2c7eb3ff095a5d4257af16b43d89f189
amitrambaran <amit.rambaran@gmail.com>
2019-02-06 23:00:53 -0500

    add comments

--- app.js
+++ app.js
@@ -1,40 +1,41 @@
 const apiKey = "e5c70d2a66ae1d10e85c6dd2081e302d";
-var data; 
+var data;
+searchQuery = document.querySelector("input[type='text']");
 
-retrieveData();
+//Add event listener to text input
+searchQuery.addEventListener("keypress", function (event) {
+    parseSearchInput(event);
+});
 
-function retrieveData(){
-    searchQuery = document.querySelector("input[type='text']");
-    searchQuery.addEventListener("keypress", function(e){
-        if(e.which === 13){
-            if(searchQuery.value === ""){
-                alert("Please enter a city.");
-            }
-            console.log(searchQuery.value);
-            let url = "http://api.openweathermap.org/data/2.5/weather?q=" + searchQuery.value + "&appid=" + apiKey;
-            //console.log(url);
-            httpRequest(url);
+//Parse text input and append to API link
+function parseSearchInput(event) {
+    if (event.which === 13) {
+        if (searchQuery.value === "") {
+            alert("Please enter a city.");
         }
-
-    });
-
+        console.log(searchQuery.value);
+        let url = "http://api.openweathermap.org/data/2.5/weather?q=" + searchQuery.value + "&appid=" + apiKey;
+        //console.log(url);
+        httpRequest(url);
+    }
 }
 
-function httpRequest(url){
+//Asyncronous AJAX request to API for 5 day / 3 hour weather info
+function httpRequest(url) {
     var httpReq = new XMLHttpRequest();
 
-    httpReq.onload = function(){
-        if (httpReq.status == 200 && httpReq.readyState == 4){
+    httpReq.onload = function () {
+        if (httpReq.status == 200 && httpReq.readyState == 4) {
             console.log("Status OK");
             data = JSON.parse(httpReq.responseText);
-            console.log(data);
-            document.getElementById("temperature").textContent = data.main.temp;
+            //console.log(data);
+            //document.getElementById("temperature").textContent = data.main.temp;
         }
-        else{
+        else {
             console.log("Status error");
         }
     }
-
+//Open a GET request with the constructed API link; send request
     httpReq.open("GET", url);
     httpReq.send();
 }
